9bf25dda7dc65463d0da7abb5f2318618f60d386
unknown <pramodkalyan28@gmail.com>
2022-10-02 07:41:43 +0530

    firebase added

--- src/src/InputOption.js
+++ src/src/InputOption.js
@@ -4,10 +4,10 @@ import './InputOption.css'
 function InputOption({Icon,title,color}) {
   return (
     <div className='inputOption'>
-
+ 
 <Icon style={{color:color}}/>
 <h4>{title}</h4>
-
+ 
     </div>
   )
 }
